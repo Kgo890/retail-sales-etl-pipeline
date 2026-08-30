c055314806f1326d99c41d790e3273177d61ddf5
Kgo890 <gorekaden501@gmail.com>
2026-08-29 21:54:50 -0700

    simplfied views to better explain them

--- postgresql/analytics/data_quality_analysis.sql
+++ postgresql/analytics/data_quality_analysis.sql
@@ -1,13 +1,3 @@
--- how many potential duplicate transactions exist
-    CREATE OR REPLACE VIEW data_quality.duplicate_count AS
-    SELECT COUNT(*) AS potential_duplicate_transactions
-    FROM data_quality_potential_duplicates;
-
--- how many potential duplicate groups exists
-    CREATE OR REPLACE VIEW data_quality.duplicate_groups AS 
-    SELECT COUNT(DISTINCT invoice_no) AS potential_duplicate_groups
-    FROM data_quality_potential_duplicates;
-
 -- what percentage of the dataset contains potential duplicates
     CREATE OR REPLACE VIEW data_quality.duplicate_percentage AS
     WITH potential_duplicates AS (
@@ -27,19 +17,3 @@
         ) AS percentage_of_potential_duplicates
     FROM potential_duplicates AS pd
     CROSS JOIN total_records AS tr;
-
--- what are the most common data_quality issues
-    CREATE OR REPLACE VIEW data_quality.issue_mix AS 
-    SELECT issue_type, 
-    COUNT(*) AS number_of_issues
-    FROM data_quality_potential_duplicates 
-    GROUP BY issue_type;
-
- -- how large are the duplicate clusters (are most duplicates simple pairs or larger groups?)
-    CREATE OR REPLACE VIEW data_quality.duplicate_cluster_sizes AS
-    SELECT 
-        duplicate_group_count,
-        COUNT(*) AS number_of_rows
-    FROM data_quality_potential_duplicates
-    GROUP BY duplicate_group_count
-    ORDER BY duplicate_group_count;
